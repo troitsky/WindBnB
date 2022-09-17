25cea18e2a7f5493b59d5a3e663a80fe678e286d
Test_user <fakemail.mail.zyx>
2022-09-17 22:34:56 +0300

    filters working but need modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useLayoutEffect  } from 'react'
 import './App.css'
 import '/logo.svg'
 import ReservationCard from './components/ReservationCard/ReservationCard'
@@ -9,43 +9,82 @@ function App() {
   const [pageOverlayShown, setpageOverlayShown] = useState(false)
   const[stays, setStays] = useState([])
   const [filterParams, setFilterParamas] = useState({city: '', country: '', guests: 0 })
+  const [locationInput, setLocationInput] = useState(null);
+  const [guestsInput, setGuestsInput] = useState(null);
   
-  function getStaysData() {
-    fetch('./data/stays.json')
-      .then(res => res.json())
-      .then(data => setStays(data))
+  async function getStaysData() {
+    const data = await fetch('./data/stays.json').then(res => res.json())
+    return data
   }
+
+  async function setStaysDataFromAPI() {
+    setStays(await getStaysData())
+  } 
 
   function guestsFilter(stay) {
     if (stay.maxGuests >= filterParams.guests) {return true}
+    console.log('guest filter fired')
     return false;
   }
   
-  useEffect(() => {
-    setpageOverlayShown(pageOverlayShown => !pageOverlayShown)
-  }, [menuOpen])
+  function cityFilter(stay) {
+    if (stay.city === filterParams.city) {return true}
+    return false;
+  }
 
-  useEffect(() => {
-    getStaysData()  
-  }, [])
-
-  useEffect(() => {
-    setStays(oldStays => oldStays.filter(guestsFilter) )  
-  }, [filterParams.guests])
-
+// Show|hide drawer menu 
   function toggleDrawerMenu() {
     setMenuOpen(menuOpen => !menuOpen)
   }
 
-  function handleInputChange(e) {
-    const filter = e.target.id
-    const value = e.target.value
-    
-    if (filter === "locationInput") {
-      setFilterParamas(oldFilterParams => ({...oldFilterParams, city: value}))
-      console.log(value)
+  // Show|hide overlay
+  useEffect(() => {
+    setpageOverlayShown(pageOverlayShown => !pageOverlayShown)
+  }, [menuOpen])
+
+// Get stays data on first render
+  useEffect( () => {
+    setStaysDataFromAPI()
+  }, [])
+
+
+  // Filter stays when  filters change but not on first render
+  useLayoutEffect(() => {
+     
+    async function updateFilters() {
+      // get stays from json and set state anew (dont forget await)
+      await setStaysDataFromAPI();
+      if (filterParams.city) {
+        setStays(oldStays => oldStays.filter(cityFilter))
+      }
+      if (filterParams.guests) {
+        setStays(oldStays => oldStays.filter(guestsFilter))
+      }       
     }
+    updateFilters()
+  }, [filterParams.city, filterParams.guests])
+
+// change guest filter when guests number change
+  useEffect(() => {
+    setFilterParamas(oldFilterParams => ({...oldFilterParams, guests: guestsInput}))
+
+  }, [guestsInput])
+
+  function handleLocationInput(e) {
+    const value = e.target.value
+    setLocationInput(value)
+    const separatedLocationInput = value.split(',')
+    const city = separatedLocationInput[0]
+    setFilterParamas(oldFilterParams => ({...oldFilterParams, city: city}))
   }
+  
+  function handleGuestsInput(e) {
+    const value = Number(e.target.value)
+    setGuestsInput(value)
+  }
+
+  function guestsIncrease() {setGuestsInput(oldGuests => oldGuests+1)}
+  function guestsDecrease() {setGuestsInput(oldGuests => oldGuests-1)}
 
   let ReservationCards = stays.map(stay => 
      <ReservationCard 
@@ -69,12 +108,21 @@ function App() {
         <div className="pageOverlay" onClick={toggleDrawerMenu} style={pageOverlayShown ? {display: "block"} : null}></div> 
 
         <div className="app_container">
-          <DrawerMenu open={menuOpen}  handleInputChange={handleInputChange} city={filterParams.city}/>
+          <DrawerMenu 
+            open={menuOpen}  
+            handleLocationInput={handleLocationInput}
+            handleGuestsInput = {handleGuestsInput}
+            city={filterParams.city}
+            locationInput={locationInput}
+            guestsInput={guestsInput}
+            guestsIncrease={guestsIncrease}
+            guestsDecrease={guestsDecrease}
+          />
           <header className='header'>
             <img src="logo.svg" alt="" className="logo" />
             <div className="btn-group">
-              <button onClick={toggleDrawerMenu}>Helsinki, Finland</button>
-              <button className='btn_add_guests' onClick={toggleDrawerMenu}>Add guests</button>
+              <button onClick={toggleDrawerMenu}>{locationInput ? locationInput : "Finland"}</button>
+              <button className='btn_add_guests' onClick={toggleDrawerMenu}>{guestsInput ? `${guestsInput} guests` : 'Add guests'}</button>
               <button className='btn btn_search' onClick={toggleDrawerMenu}><i className="fa-solid fa-magnifying-glass"></i></button>
             </div>
           </header>
@@ -82,7 +130,7 @@ function App() {
           <main className='main_content'>
             <div className="main_section_topbar">
               <h2 className="main_title">Stays in Finland</h2>
-              <span>12+ stays</span>
+              <span>{stays.length} stays</span>
             </div>
             <div className="cards_container">
               {ReservationCards}
--- src/components/DrawerMenu/DrawerMenu.jsx
+++ src/components/DrawerMenu/DrawerMenu.jsx
@@ -1,55 +1,105 @@
-import './DrawerMenu.css'
+import "./DrawerMenu.css";
 
 export default function DrawerMenu(props) {
-    
-    const {open, handleInputChange, city} = props;
-    return (
-        <nav className="drawer_menu" style={open ? { transform: "none"} : null}>
-          <div className="search_options_bar">
-            <div className="drawer_input_background" style={{borderColor: "#333333"}}>
-              <label htmlFor="locationInput" className='drawer_text_input_label'>Location</label>
-              <input id="locationInput" className="drawer_text_input" onChange={handleInputChange} type="text" value={city} placeholder="Helsinki, Finland" />
-            </div>
-            <div className="drawer_input_background" >
-              <label htmlFor="locationInput" className='drawer_text_input_label'>Guests</label>
-              <input id="locationInput" className="drawer_text_input" type="text" value="Add guests" />
-            </div>
-            <div className="drawer_input_background">
-              <button className='btn btn_search_red'><i className="fa-solid fa-magnifying-glass"></i>Search</button>
+  const { open, handleLocationInput, handleGuestsInput, locationInput, guestsInput, guestsIncrease, guestsDecrease} = props;
+  return (
+    <nav className="drawer_menu" style={open ? { transform: "none" } : null}>
+      <div className="search_options_bar">
+        <div
+          className="drawer_input_background"
+          style={{ borderColor: "#333333" }}
+        >
+          <label htmlFor="locationInput" className="drawer_text_input_label">
+            Location
+          </label>
+          <input
+            id="locationInput"
+            className="drawer_text_input"
+            onChange={handleLocationInput}
+            type="text"
+            value={locationInput}
+            placeholder="Helsinki, Finland"
+          />
+        </div>
+        <div className="drawer_input_background">
+          <label htmlFor="locationInput" className="drawer_text_input_label">
+            Guests
+          </label>
+          <input
+            id="guestsInput"
+            className="drawer_text_input"
+            type="number"
+            placeholder="Add guests"
+            onChange={handleGuestsInput}
+            value = {guestsInput}
+          />
+        </div>
+        <div className="drawer_input_background">
+          <button className="btn btn_search_red">
+            <i className="fa-solid fa-magnifying-glass"></i>Search
+          </button>
+        </div>
+      </div>
+      <div className="search_options_setting_containers">
+        <div className="location_search_results_container">
+          <ul className="location_search_results_list">
+            <li className="location_search_result_item">
+              <i className="fa-solid fa-location-dot"></i>Helskinki, Finland
+            </li>
+            <li className="location_search_result_item">
+              <i className="fa-solid fa-location-dot"></i>Helskinki, Finland
+            </li>
+            <li className="location_search_result_item">
+              <i className="fa-solid fa-location-dot"></i>Helskinki, Finland
+            </li>
+            <li className="location_search_result_item">
+              <i className="fa-solid fa-location-dot"></i>Helskinki, Finland
+            </li>
+          </ul>
+        </div>
+
+        <div className="guest_search_settings_container">
+          <div className="guest_settings_block">
+            <p className="guest_settings_title">Adults</p>
+            <p className="guest_settings_subtitle">Ages 13 or above</p>
+            <div className="quantity-part">
+              <div className="btn btn-plus-minus" onClick={guestsDecrease}>
+                <i className="fa-solid fa-minus"></i>
+              </div>
+              <input
+                type="number"
+                className="guest-qty"
+                value="0"
+                min="0"
+                id=""
+              />
+              <div className="btn btn-plus-minus" onClick={guestsIncrease}>
+                <i className="fa-solid fa-plus"></i>
+              </div>
             </div>
           </div>
-          <div className="search_options_setting_containers">
-            <div className="location_search_results_container">
-              <ul className='location_search_results_list'>
-                <li className="location_search_result_item"><i className="fa-solid fa-location-dot"></i>Helskinki, Finland</li>
-                <li className="location_search_result_item"><i className="fa-solid fa-location-dot"></i>Helskinki, Finland</li>
-                <li className="location_search_result_item"><i className="fa-solid fa-location-dot"></i>Helskinki, Finland</li>
-                <li className="location_search_result_item"><i className="fa-solid fa-location-dot"></i>Helskinki, Finland</li>
-              </ul>
-            </div>
-
-            <div className="guest_search_settings_container">
-              <div className="guest_settings_block">
-                <p className="guest_settings_title">Adults</p>
-                <p className="guest_settings_subtitle">Ages 13 or above</p>
-                <div className="quantity-part">
-                    <div className="btn btn-plus-minus"><i className="fa-solid fa-minus"></i></div>
-                    <input type="number" className="guest-qty" value="0" min="0" id="" />
-                    <div className="btn btn-plus-minus"><i className="fa-solid fa-plus"></i></div>
-                </div>
+          <div className="guest_settings_block">
+            <p className="guest_settings_title">Children</p>
+            <p className="guest_settings_subtitle">Ages 2-12</p>
+            <div className="quantity-part">
+              <div className="btn btn-plus-minus" onClick={guestsDecrease}>
+                <i className="fa-solid fa-minus"></i>
               </div>
-              <div className="guest_settings_block">
-                <p className="guest_settings_title">Children</p>
-                <p className="guest_settings_subtitle">Ages 2-12</p>
-                <div className="quantity-part">
-                    <div className="btn btn-plus-minus"><i className="fa-solid fa-minus"></i></div>
-                    <input type="number"  className="guest-qty" value="0" min="0" id="" />
-                    <div className="btn btn-plus-minus"><i className="fa-solid fa-plus"></i></div>
-                </div>
+              <input
+                type="number"
+                className="guest-qty"
+                min="0"
+                placeholder="0"
+                id=""
+                
+              />
+              <div className="btn btn-plus-minus" onClick={guestsIncrease}>
+                <i className="fa-solid fa-plus"></i>
               </div>
             </div>
           </div>
-
-        </nav>
-    )
+        </div>
+      </div>
+    </nav>
+  );
 }
